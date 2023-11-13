f2d76072c4a03b0ecaf846b9ebd6ce781412fa01
Cauã de França Brito <cauafranca115@gmail.com>
2023-11-13 19:35:53 -0300

    add excluir

--- src/app/components/adm-product-card/adm-product-card.component.ts
+++ src/app/components/adm-product-card/adm-product-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { UserService } from '../../services/user.service'; // Substitua pelo caminho real
 
 @Component({
   selector: 'app-adm-product-card',
   templateUrl: './adm-product-card.component.html',
-  styleUrl: './adm-product-card.component.css'
+  styleUrls: ['./adm-product-card.component.css']
 })
 export class AdmProductCardComponent {
   @Input() data: any;
@@ -18,10 +18,35 @@ export class AdmProductCardComponent {
   @Input() imgProduto: string = '';
   @Input() qtdProduto: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   redirecionarEditPage() {
     this.router.navigate(['/edit-product']);
   }
 
+  excluirProduto() {
+    // Certifique-se de que data é definido e possui a propriedade idProduto
+    if (this.data && this.data.idProduto) {
+      const idProduto = this.data.idProduto;
+
+      this.userService.excluirProduto(idProduto).subscribe(
+        (response) => {
+          if (response.status === 204) {
+            console.log('Produto excluído com sucesso', response);
+            // Recarregar a página após a exclusão
+            this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+            this.router.onSameUrlNavigation = 'reload';
+            this.router.navigate([this.router.url]);
+          } else {
+            console.error('Erro ao excluir produto - Resposta inesperada:', response);
+          }
+        },
+        (error) => {
+          console.error('Erro ao excluir produto', error);
+        }
+      );
+    } else {
+      console.error('Objeto de produto inválido:', this.data);
+    }
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,8 +9,8 @@ import { Observable } from 'rxjs';
 export class UserService {
   private createUser = 'http://localhost:8090/usuario';
   private createProduto = 'http://localhost:8090/produtos';
-  private getProdutos = 'http://localhost:8090/produtos';  // Adicione a URL para obter produtos
-
+  private getProdutos = 'http://localhost:8090/produtos';
+  private deleteProduto = 'http://localhost:8090/produtos';  // Adicione a URL para excluir produtos
 
   constructor(private http: HttpClient) { }
 
@@ -25,5 +25,11 @@ export class UserService {
   obterProdutos(): Observable<any> {
     return this.http.get(this.getProdutos);
   }
-  
+
+  excluirProduto(productId: number): Observable<any> {
+    // Adicione lógica para enviar uma solicitação de exclusão
+    // Pode ser necessário passar o ID ou outro identificador único do produto
+    // Exemplo: return this.http.delete(`${this.deleteProduto}/${productId}`);
+    return this.http.delete(`${this.deleteProduto}/${productId}`);
+  }
 }
